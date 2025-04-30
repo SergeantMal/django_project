c912f2f6bb87842a06494bf93e961ede00cd17da
SergeantMal <dersergeant@gmail.com>
2025-04-30 19:17:27 +0300

    add admin panel + blog

--- django_lesson/blog/admin.py
+++ django_lesson/blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import blog_post
+
+admin.site.register(blog_post)
+
 # Register your models here.
--- django_lesson/blog/views.py
+++ django_lesson/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import blog_post
 
 # Create your views here.
 def blog(request):
+    news = blog_post.objects.all()
     return render(request, 'blog/blog.html', {
-        'title': '<strong>Блог</strong>'})
+        'title': '<strong>Блог</strong>', 'news': news})
