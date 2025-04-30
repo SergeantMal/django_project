23eb221cc3c220b46a1e3aa1d236d88c755a5f24
SergeantMal <dersergeant@gmail.com>
2025-04-30 19:32:49 +0300

    Update views.py

--- django_lesson/blog/views.py
+++ django_lesson/blog/views.py
@@ -3,6 +3,6 @@ from .models import blog_post
 
 # Create your views here.
 def blog(request):
-    news = blog_post.objects.all()
+    news = blog_post.objects.all().order_by('-pub_date')
     return render(request, 'blog/blog.html', {
         'title': '<strong>Блог</strong>', 'news': news})
